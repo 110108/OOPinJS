945bee4eb7246a453c675b369843d8ea5a9ee315
110108 <31996737+110108@users.noreply.github.com>
2017-11-16 15:54:25 -0500

    fixed some stuff, added other stuff

--- sketch.js
+++ sketch.js
@@ -1,39 +1,78 @@
 let bubbles=[];
+let player;
+let gamestate;
+let canvasHeight;
 
 function setup() { // built-in P5.JS function -=- this runs once
-	createCanvas(windowWidth, windowHeight);
-	for(let i=0; i<10; i++){
-		let x=random(width);
+	createCanvas((windowWidth-20), (windowHeight-20));
+	for(let i=0; i<random(10,500); i++){
+		let x=random(10,(width-10));
 		let y=random(height);
 		let r=random(10,50);
 		let b=new Bubble(x, y, r);
 		bubbles.push(b);
 	}
+	player=new ship(0,0);
+	gamestate="title";
+	console.log(windowHeight-20);
 }
 
 
-function mouseClicked(){
-	for (let i=0; i<bubbles.length; i++){
-		bubbles[i].clicked();
-	}
-}
-
-/*function mouseDragged() {
-	let r=random(10, 50);
-	let b=new Bubble(mouseX, mouseY, r);
-	bubbles.push(b);
-}*/
 
 
 function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
 	background(0); // give the canvas a black background
-	for (let i=0; i<bubbles.length; i++){
-		bubbles[i].move();
-		bubbles[i].show();
-		//bubbles[i].clicked();
+	//if(gamestate=="ingame"){
+		for (let i=0; i<bubbles.length; i++){
+			bubbles[i].move();
+			bubbles[i].show();
+			bubbles[i].warp();
+		}
+		player.move();
+	 	player.show();
+	//}
+
+	// else if(gamestate=="loss"){
+	// 	//print loss screen
+	// }
+  //
+	// else if(gamestate=="win"){
+	// 	//print win screen
+	// }
+}
+
+function mousePressed(){
+	if(gamestate=="title"){
+		gamestate="ingame";
 	}
-	//bubble.move();
-	//bubble.show();
+	else if(gamestate=="loss"){
+		gamestate="title";
+	}
+	else if (gamestate=="win") {
+		gamestate="title";
+	}
+	else if(gamestate=="ingame"){
+		//make the bullets?
+	}
+}
+
+class ship {
+	constructor(x,y){
+		this.x=x;
+		this.y=y;
+	}
+
+	move(){
+		this.x=mouseX;
+		this.y=(windowHeight-45);//subject to change
+	}
+
+	show(){
+		stroke(255); // white outline
+		strokeWeight(1); // line width
+		fill(random(10,255),0,0);
+		triangle((this.x-20), (this.y+10), (this.x), (this.y-10), (this.x+20), (this.y+10));
+	}
 }
 
 class Bubble {
@@ -43,33 +82,23 @@ class Bubble {
 		this.r=r;
 	}
 
-	clicked() {
-		let d=dist(mouseX, mouseY, this.x, this.y);
-		if(d<this.r){
-			console.log("Clicked On Bubble");
-		}
-	}
-
 	move() {
-	this.x = this.x + random(-5,5);
-	this.y = this.y + random(0,5);
-
-	if(this.y==windowHeight){
-		this.y=0;
-		console.log("y=0")
+		this.x = this.x + random(-5,5);
+		this.y = this.y + random(0,5);
 	}
 
-	if(this.x==windowWidth){
-		this.x-=1;
-		console.log("x=0")
-	}
+	warp() {
+		//if(this.y==(700)){
+			//this.y=10;
+			console.log("y=0")
+		//}
 	}
 
 	show() {
-	stroke(255); // white outline
-	strokeWeight(4); // line width
-	noFill();
-	ellipse(this.x, this.y, this.r, this.r); // draw an ellipse/circle
+		stroke(255); // white outline
+		strokeWeight(4); // line width
+		fill(random(0,100),random(1,255),random(1,255));
+		ellipse(this.x, this.y, this.r, this.r);
 	}
 }
 //2.30
