55e04761a3b5902c8a02ef99a2fa0bfb958d1fd0
110108 <31996737+110108@users.noreply.github.com>
2017-11-25 12:32:36 -0500

    pretty much the finished game

--- sketch.js
+++ sketch.js
@@ -2,30 +2,35 @@ let bubbles=[];
 let player;
 let gamestate;
 let timer;
-let score;
+let bg;
+let ws;
+let ls;
 
 function setup() { // built-in P5.JS function -=- this runs once
 	createCanvas((windowWidth-20), (windowHeight-20));
-	for(let i=0; i<random(10,500); i++){
-		let x=random(10,(width-10));
-		let y=random(height);
-		let r=random(10,50);
-		let b=new Bubble(x, y, r);
-		bubbles.push(b);
-
-	}
 	player=new ship(0,0);
 	gamestate="title";
+	score=0;
+	bg=loadImage("https://cdn.glitch.com/f3153797-32fb-4ded-bb9d-20975e419671%2Fgame-dev-startscreen-01.png?1511628691331");
+	ws=loadImage("https://cdn.glitch.com/f3153797-32fb-4ded-bb9d-20975e419671%2Fws.png?1511629985411");
+	ls=loadImage("https://cdn.glitch.com/f3153797-32fb-4ded-bb9d-20975e419671%2Fls.png?1511630368173");
 }
 
 function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
-	background(0); // give the canvas a black background
 	if(gamestate=="title"){
+		background(bg);
+		for(let i=0; i<random(5,500); i++){
+			let x=random(10,(width-10));
+			let y=random(height);
+			let r=random(30,50);
+			let b=new Bubble(x, y, r);
+			bubbles.push(b);
+		}
 		timer=6000;
-		score=0;
-		//print title W/ epilepsy warning
 	}
+
 	else if(gamestate=="ingame"){
+		background(0);
 		for (let i=0; i<bubbles.length; i++){
 			bubbles[i].move();
 			bubbles[i].show();
@@ -33,30 +38,41 @@ function draw() { // built-in P5.JS function -=-  automatic loop that repeats fo
 			bubbles[i].checkX(windowWidth-20);
 		}
 
-		text((timer/100)+"seconds",50,40)
+		for(let i=0; i<bubbles.length; i++){
+			let d=dist(bubbles[i].x,bubbles[i].y,player.x,player.y)
+			if(d<bubbles[i].r){
+				bubbles.splice(i,1)
+			}
+		}
+
+		text((timer/100)+" seconds",50,40)
 		timer--;
+		set++;
 
 		player.move();
 	 	player.show();
 
-		if(timer==0&&score>10){
-			gamestate="win";
-		}
-		else if (timer==0&&score<10){
+		if (timer==0&&bubbles.length>=0){
 			gamestate="loss";
+		}
+		else if(bubbles.length==0){
+			gamestate="win";
 		}
 	}
 
 	else if(gamestate=="loss"){
-		//print loss screen
+		background(ls);
 	}
 
 	else if(gamestate=="win"){
-		//print win screen
+		background(ws);
 	}
 }
 
 function mousePressed(){
+	if(gamestate=="start"){
+		gamestate="title"
+	}
 	if(gamestate=="title"){
 		gamestate="ingame";
 	}
@@ -88,6 +104,10 @@ class ship {
 		fill(random(10,255),0,0);
 		triangle((this.x-20), (this.y+10), (this.x), (this.y-10), (this.x+20), (this.y+10));
 	}
+
+	// intersects(that){
+	// 	if()
+	// }
 }
 
 class Bubble {
@@ -99,7 +119,7 @@ class Bubble {
 
 	move() {
 		this.x = this.x + random(-5,5);
-		this.y = this.y + random(0,5);
+		this.y = this.y + random(0,10);
 	}
 
 	show() {
